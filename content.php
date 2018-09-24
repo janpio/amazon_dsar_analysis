070453aa27e913ffe42978d1a6de75d72e8eb0f8
Jan Piotrowski <piotrowski+github@gmail.com>
2018-09-24 18:52:23 +0200

    output scraped asin titles

--- content.php
+++ content.php
@@ -9,13 +9,16 @@ $folder = $path.'/'.$foldername;
 $files = scandir($folder);
 $files = array_diff($files, array('.', '..'));
 
+$asinCache = file_get_contents('cache/asins.json');
+$asins = json_decode($asinCache, true);
+
 foreach($files as $file) {
     $content = file_get_contents($folder.'/'.$file);
     $array = json_decode($content, true);
     $items[] = [
         'type' => $array['resource']['resourceType'],
         'asin' => $array['resource']['resourceId'],
-        'name' => '',
+        'name' => $asins[$array['resource']['resourceId']],
         'acquired' => $array['rights'][0]['acquiredDate'],
         'parent' => $array['resource']['parent']['resourceId'],
     ];    
@@ -25,5 +28,13 @@ echo "<table>";
 echo "<tr><td>Type</td><td>ASIN</td><td>Name</td><td>Acquired</td><td>Parent</td></tr>\n";
 foreach($items as $item) {
     echo "<tr><td>".$item['type']."</td><td>".$item['asin']."</td><td>".$item['name']."</td><td>".$item['acquired']."</td><td>".$item['parent']."</td></tr>\n";
+    if(!$asins[$item['asin']]) {
+        $asins[$item['asin']] = '';
+    }
 }
 echo "</table>";
+
+print_r($asins);
+$asinCache = json_encode($asins, JSON_PRETTY_PRINT);
+echo $asinCache;
+file_put_contents('cache/asins.json', $asinCache);
